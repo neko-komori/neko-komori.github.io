aa8f55fd97631bcd4f6049fd093aa5f153b5bbbd
Neko Komori <53139691+neko-komori@users.noreply.github.com>
2019-07-28 21:03:07 +0700

    Delete neko-komori.js

--- neko-komori.js
+++
@@ -1,9 +0,0 @@
-var neko = {
-	posisi : "",
-	melacakPosisi : function (callback){
-		navigator.geolocation.getCurrentPosition(function (position){
-			this.posisi = position.coords.latitude+" "+position.coords.longitude;
-			callback();
-		});
-	}
-}
