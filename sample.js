2051432d82609d00180637ae5cf67b4d094acdb5
Neko Komori <53139691+neko-komori@users.noreply.github.com>
2019-09-12 14:38:39 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,27 +1,14 @@
 function load(){
-
 document.getElementById("demo").style.color = "red";
-
 var ajax = new XMLHttpRequest();
-
 ajax.onreadystatechange = function (){
-
 if(this.readyState == "4" && this.status == "200"){
-
 document.getElementById("demo").innerHTML = this.responseText;
-
 }
-
 else{
-
 document.getElementById("demo").innerHTML = this.readyState+" masih ambil data "+this.status;
-
 }
-
 }
-
-ajax.open("GET", "https://scr-cdn.000webhostapp.com/encodedecodejsjson.php", true);
-
+ajax.open("GET", "http://scr-cdn.000webhostapp.com/encodedecodejsjson.php", true);
 ajax.send();
-
 }
