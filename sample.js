a5e1b54d4c01b2ea76149da6b341a9ffaabfbae5
Neko Komori <53139691+neko-komori@users.noreply.github.com>
2019-09-12 14:41:46 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -2,12 +2,7 @@ function load(){
 document.getElementById("demo").style.color = "red";
 var ajax = new XMLHttpRequest();
 ajax.onreadystatechange = function (){
-if(this.readyState == "4" && this.status == "200"){
-document.getElementById("demo").innerHTML = this.responseText;
-}
-else{
 document.getElementById("demo").innerHTML = this.readyState+" masih ambil data "+this.status;
-}
 }
 ajax.open("GET", "http://scr-cdn.000webhostapp.com/encodedecodejsjson.php", true);
 ajax.send();
