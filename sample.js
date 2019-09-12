8ba0e1a37fd799fd818d4b7727f6992ec049f6c4
Neko Komori <53139691+neko-komori@users.noreply.github.com>
2019-09-12 14:35:54 +0000

    Update sample.js

--- sample.js
+++ sample.js
@@ -6,7 +6,7 @@ var ajax = new XMLHttpRequest();
 
 ajax.onreadystatechange = function (){
 
-if(this.readyState == "4" && thi.status == "200"){
+if(this.readyState == "4" && this.status == "200"){
 
 document.getElementById("demo").innerHTML = this.responseText;
 
