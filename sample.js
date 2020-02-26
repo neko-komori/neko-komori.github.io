925e3b6ad7f13961045064f1d67c585b7fc62233
Neko Komori <53139691+neko-komori@users.noreply.github.com>
2020-02-26 17:08:08 +0000

    Delete sample.js

--- sample.js
+++
@@ -1,9 +0,0 @@
-function load(){
-document.getElementById("demo").style.color = "red";
-var ajax = new XMLHttpRequest();
-ajax.onreadystatechange = function (){
-document.getElementById("demo").innerHTML = this.readyState+" masih ambil data "+this.status;
-}
-ajax.open("GET", "http://scr-cdn.000webhostapp.com/encodedecodejsjson.php", true);
-ajax.send();
-}
